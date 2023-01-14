953749c8097ea253bc50e9ca90eecacd1abf1f3b
Sergio <43445037+sergioandresco@users.noreply.github.com>
2023-01-14 11:31:48 -0500

    Finalizacion de diseño y componentes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import {Testimonio} from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Pagina web con React</h1>
+        <Testimonio 
+          nombre='Sergio Cobos'
+          pais='Colombia'
+          imagen='sergio'
+          cargo='Ingeniero de Software'
+          testimonio='Primer desarrollo WEB utilizando REACT, para conocer una nueva tecnología.'
+          />
+          <Testimonio 
+          nombre='Sergio IA'
+          pais='Realidad Virtual'
+          imagen='sergioIA'
+          cargo='Desarrollador Web'
+          testimonio='Soy una creación con IA, con ayuda de Python y par de librerías extras.'
+          />
+          <Testimonio 
+          nombre='Lionel Andres Messi Cuccitini'
+          pais='Argentina'
+          imagen='messi'
+          cargo='Capitan de la selección Argentina'
+          testimonio='Después de tantas derrotas, por fin somos campeónes del mundo, por fin llego el gran sueño y la tercera para Argentina'
+          />
+      </div>
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import '../estilos/Testimonio.css';
 
-export function Testimonio() {
+export function Testimonio(props) {
   return ( 
     <div className = 'contenedor-testimonio' >
       <img 
         className = 'imagen-testimonio'
-        src = {require('../imagenes/testimonio-sergio.JPEG')}
+        src = {require(`../imagenes/testimonio-${props.imagen}.JPEG`)}
         alt='Foto de Sergio' 
       />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Primera pagina web con React</p>
-        <p className='cargo-testimonio'>Ingeniero de Sistemas</p>
-        <p className='texto-testimonio'>Esta es la primera prueba de creación de una pagina web utilizando React y JavaScript</p>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> de {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          <strong>{props.cargo}</strong>
+        </p>
+        <p className='texto-testimonio'>
+          {props.testimonio}
+        </p>
       </div>
     </div>
   );
